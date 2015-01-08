49eb3094d8cb982232f4dc83054093827436ab57
Peter Mouland <peter.mouland@gmail.com>
2015-01-08 14:05:41 +0000

    half finished spike into screen shotting

--- gulpfile.js
+++ gulpfile.js
@@ -10,93 +10,115 @@ var spawn = child_process.spawn;
 var exec = child_process.exec;
 
 
-//gulp.task('screenshot:record', function(cb){
-//
-//    connect.server({
-//        root: '_site',
-//        port: 8888
-//    });
-//
-//    var casperChild = spawn('casperjs', ['test/casper.js'].concat(['test/casper.js']));
-//
-//    casperChild.on('close', function (code) {
-//        var success = code === 0; // Will be 1 in the event of failure
-//
-//        if (success){
-//
-//        } else {
-//
-//        }
-//
-//
-//        connect.serverClose();
-//        gulp.emit('end', cb)
-//    });
-//
-//
-//});
-//
-//
-//gulp.task('screenshot:accepted', function(cb){
-//    return gulp.src('test/screenshots/new/*')
-//        .pipe(gulp.dest('test/screenshots/reference'))
-//});
-//
-//var resemble = require('resemblejs');
-//gulp.task('screenshot:compare', function(cb){
-//    var api = resemble('test/screenshots/reference/google.png').resemble('test/screenshots/new/google.png').onComplete(function(data){
-//        console.log(data);
-//        /*
-//         {
-//         red: 255,
-//         green: 255,
-//         blue: 255,
-//         brightness: 255
-//         }
-//         */
-//    });
-//});
+gulp.task('screenshot:record', function(cb){
 
-gulp.task('init:screenshot', function(cb){
-
-    var npmChild = spawn('npm', ['install'], {cwd: 'bower_components/backstopjs'});
-    npmChild.on('close', function (code) {
-        var success = code === 0; // Will be 1 in the event of failure
-        if (success){  } else {     }
-
-        gulp.emit('end', cb)
+    connect.server({
+        root: '_site',
+        port: 8888
     });
+
+    var casperChild = spawn('casperjs', ['test/casper.js'].concat(['test/casper.js']));
+
+    casperChild.on('close', function (code) {
+        var success = code === 0; // Will be 1 in the event of failure
+
+        if (success){
+
+        } else {
+
+        }
+
+
+        connect.serverClose();
+        cb();
+    });
+
 
 });
 
-gulp.task('screenshot:test', function(cb){
 
-    var gulpChild = spawn('gulp', ['test'], {cwd: 'bower_components/backstopjs'});
-    gulpChild.on('close', function (code) {
-        console.log(arguments);
-        var success = code === 0; // Will be 1 in the event of failure
-        if (success){
-            console.log('success!');
-            gulp.src('./bower_components/backstopjs/bitmaps_reference/**/*')
-                .pipe(gulp.dest('./test/screenshots/reference'))
-                .emit('end', cb);
+gulp.task('screenshot:accepted', function(cb){
+    return gulp.src('test/screenshots/new/*')
+        .pipe(gulp.dest('test/screenshots/reference'))
+});
+
+var fs = require('fs');
+var resemble = require('node-resemble');
+gulp.task('screenshot:compare', function(cb){
+    try {
+        var img1 = fs.readFileSync('./test/screenshots/reference/body.png');
+    } catch(e){
+        console.log('The reference shot does not exit. Please copy it across.');
+        process.exit(1);
+    }
+    var img2 = fs.readFileSync('./test/screenshots/new/body.png');
+    var api = resemble(img2).compareTo(img1).onComplete(function(data){
+        var err = false;
+        if (!data.isSameDimensions) {
+            if (data.dimensionDifference.width !== 0) {
+                console.log('the new image is wider/smaller: ' + data.dimensionDifference.width + 'px different');
+            }
+            if (data.dimensionDifference.height !== 0) {
+                console.log('the new image is taller/smaller: ' + data.dimensionDifference.height + 'px different');
+            }
+            err = true;
+        }
+        if (data.misMatchPercentage > 0) {
+            console.log('The new image content has changed: ' + data.misMatchPercentage + '% different');
+            console.log('Please check the following images and move them to reference directory if they are correct.');
+            console.log(__dirname + '/test/screenshots/new/google.png');
+            err = true;
+        }
+        if (err){
+            process.exit(1);
         } else {
-            console.log('woah!');
-            gulp.emit('end', cb)
+            console.log('The new images match the reference shots');
+            cb();
         }
     });
 
-
-    gulpChild.stdout.on('data', function (data) {
-        console.log('stdout: ' + data);
-    });
-
-    gulpChild.stderr.on('data', function (data) {
-        console.log('stderr: ' + data);
-    });
-
-    gulpChild.on('close', function (code) {
-        console.log('child process exited with code ' + code);
-    });
-
 });
+
+//gulp.task('init:screenshot', function(cb){
+//
+//    var npmChild = spawn('npm', ['install'], {cwd: 'bower_components/backstopjs'});
+//    npmChild.on('close', function (code) {
+//        var success = code === 0; // Will be 1 in the event of failure
+//        if (success){  } else {     }
+//
+//        gulp.emit('end', cb)
+//    });
+//
+//});
+//
+//gulp.task('screenshot:test', function(cb){
+//
+//    var gulpChild = spawn('gulp', ['test'], {cwd: 'bower_components/backstopjs'});
+//    gulpChild.on('close', function (code) {
+//        console.log(arguments);
+//        var success = code === 0; // Will be 1 in the event of failure
+//        if (success){
+//            console.log('success!');
+//            gulp.src('./bower_components/backstopjs/bitmaps_reference/**/*')
+//                .pipe(gulp.dest('./test/screenshots/reference'))
+//                .emit('end', cb);
+//        } else {
+//            console.log('woah!');
+//            gulp.emit('end', cb)
+//        }
+//    });
+//
+//
+//    gulpChild.stdout.on('data', function (data) {
+//        console.log('stdout: ' + data);
+//    });
+//
+//    gulpChild.stderr.on('data', function (data) {
+//        console.log('stderr: ' + data);
+//    });
+//
+//    gulpChild.on('close', function (code) {
+//        console.log('child process exited with code ' + code);
+//    });
+//
+//});
--- test/casper.js
+++ test/casper.js
@@ -5,7 +5,7 @@ var casper = require('casper').create({
 
 
 casper.start('http://localhost:8888/', function() {
-    this.captureSelector('test/screenshots/new/google.png', 'body');
+    this.captureSelector('test/screenshots/new/body.png', 'body');
 });
 
 casper.on('capture.saved', function(err) {
