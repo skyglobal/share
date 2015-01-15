c11f2782d609fd2a4ad7f94bce6b6b253f61396c
Peter Mouland <peter.mouland@gmail.com>
2015-01-15 12:23:08 +0000

    update sheut + test on circle

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,19 @@ var componentHelper = require('gulp-component-helper')(gulp);
 var paths = componentHelper.paths;
 var sheut = require('sheut');
 
-gulp.task('sheut:clean', function(cb){
+gulp.task('sheut:clean', function(){
     return sheut.clean();
 });
 
-gulp.task('sheut:capture', ['clean'], function(cb){
+gulp.task('sheut:capture', ['sheut:clean'], function(){
     return sheut.capture();
 });
 
-gulp.task('sheut:accept', function(cb){
+gulp.task('sheut:accept', function(){
     return sheut.accept();
 });
 
-gulp.task('sheut:compare', ['capture'], function(cb){
+gulp.task('sheut:compare', ['sheut:capture'], function(){
     return sheut.compare()
         .then(function onSuccess(){
 
