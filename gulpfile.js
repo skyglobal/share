c5e5c11360af8051532bc9571dfbf3c4db73bb4d
Peter Mouland <peter.mouland@gmail.com>
2015-01-15 15:42:22 +0000

    improve messaging

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,30 @@
 'use strict';
 
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var componentHelper = require('gulp-component-helper')(gulp);
 var paths = componentHelper.paths;
 var sheut = require('sheut');
 
+function error(err){
+    gulp.emit("error", err);
+}
+function success(err){
+    gutil.log(gutil.colors.green(err.message));
+}
+
 gulp.task('sheut:clean', function(){
-    return sheut.clean();
+    return sheut.clean().then(success, error);
 });
 
 gulp.task('sheut:capture', ['sheut:clean'], function(){
-    return sheut.capture();
+    return sheut.capture().then(success, error);
 });
 
 gulp.task('sheut:accept', function(){
-    return sheut.accept();
+    return sheut.accept().then(success, error);
 });
 
 gulp.task('sheut:compare', ['sheut:capture'], function(){
-    return sheut.compare()
-        .then(function onSuccess(){
-
-        }, function onError(err){
-            gulp.emit("error", err);
-        });
+    return sheut.compare().then(success, error);
 });
